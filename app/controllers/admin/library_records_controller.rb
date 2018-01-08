bf85c44f9909975d6acb86271c825d0fc53165d2
shriyesh pujari <shriyesh@techverito.com>
2018-01-08 15:06:40 +0530

    Test for library_records controller

--- app/controllers/admin/library_records_controller.rb
+++ app/controllers/admin/library_records_controller.rb
@@ -2,7 +2,13 @@ class Admin::LibraryRecordsController < Admin::SessionsController
   before_action :user_logged
 
   def index
-    @records = LibraryRecord.all
+    if params[:filter].eql? "borrowed"
+      @records = LibraryRecord.includes(:book).where(returned: false)
+    elsif params[:filter].eql? "returned"
+      @records = LibraryRecord.includes(:book).where(returned: true)
+    else
+      @records = LibraryRecord.includes(:book).all
+    end
   end
 
 end
--- test/controllers/admin/library_records_controller_test.rb
+++ test/controllers/admin/library_records_controller_test.rb
@@ -14,7 +14,23 @@ class Admin::LibraryRecordsControllerTest < ActionDispatch::IntegrationTest
   test "index_should_return_records_instance_on_authenticated" do 
     post session_path, params: { user: {email: 'rose@gmail.com', password: 'rose123'} } 
     get admin_library_records_path
-    assert @controller.instance_variable_get(:@records).first.eql? @record
+    assert controller.instance_variable_get(:@records).first.eql? @record
+    assert_equal "text/html; charset=utf-8", response['content-type']
   end
+
+  test "index_should_return_records_of_borrowed_books" do
+    post session_path, params: { user: {email: 'rose@gmail.com', password: 'rose123'} }
+    get admin_library_records_path, params: { filter: "borrowed" }, xhr: true
+    assert_equal 2, controller.instance_variable_get(:@records).count 
+    assert_equal "text/javascript; charset=utf-8", response['content-type']
+  end
+
+  test "index_should_return_records_of_returned_books" do
+    post session_path, params: { user: {email: 'rose@gmail.com', password: 'rose123'} }
+    get admin_library_records_path, params: { filter: "returned" }, xhr: true
+    assert_equal 2, @controller.instance_variable_get(:@records).count 
+    assert_equal "text/javascript; charset=utf-8", response['content-type']
+  end
+
   
 end
